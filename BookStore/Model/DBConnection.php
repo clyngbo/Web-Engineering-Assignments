57bc7328dde39f02ae39d6b07688c80000d92a4e
lyngbo1109 <31726129+lyngbo1109@users.noreply.github.com>
2017-10-19 18:05:10 +0200

    dbconnection

--- BookStore/Model/DBConnection.php
+++ BookStore/Model/DBConnection.php
@@ -6,3 +6,37 @@
  * and open the template in the editor.
  */
 
+class DBConnection{
+    private $server = "localhost";
+    private $port = "3306";
+    private $password = "";
+    private $user = "root";
+    private $database = "bookstore";
+    private $conn;
+    public function __construct() {
+        $this->$conn = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
+    }
+    
+    public function query($sql)
+    {
+        try{
+            
+            if($this->$conn->connect_error)
+            {
+                die("Connection failed: ". $this->$conn->connect_errno);
+            }
+            return $result = $this->$conn->query($sql);
+        } 
+        catch (Exception $ex) {
+            die("DBConnectyion failed: ".$ex->getMessage());
+        }
+         finally {
+            $this->$conn->close();    
+                }
+    }
+    
+    public function statement($sql)
+    {
+        
+    }
+}
