bfe76d8b6023705192ad7b456154ee1b73561744
lyngbo1109 <31726129+lyngbo1109@users.noreply.github.com>
2017-10-26 11:19:30 +0200

    Reports
    Reports from earlier semesterprojects

--- BookStore/Control/Book_controller.php
+++ BookStore/Control/Book_controller.php
@@ -1,5 +1,9 @@
 <?php
-
+include_once '../Model/Book.php';
+include_once '../Model/Category.php';
+include_once '../Model/Order.php';
+include_once '../Model/Manager.php';
+include_once '../Model/BookOrder.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,5 +16,66 @@
  * @author Christian
  */
 class Book_controller {
-    //put your code here
+    private $conn;
+    public function __construct() {
+        $this->conn= new DBConnection();
+    }
+    
+    public function getAllBooks()
+    {
+        $sql = "SELECT id FROM book";
+        $books = new ArrayObject();
+        $result = $this->conn->query($sql);
+        while ($row = $result->fetch_assoc())
+        {
+            $id = $row['id'];
+            $book = new Book();
+            $book->loadBookByID($id);
+            $books->append($book);
+        }
+        return $books;
+    }
+    
+    public function getBook($id)
+    {
+        $book = new Book();
+        $book->loadBookByID($id);
+        return $book;
+    }
+    
+    public function createBook(Book $book)
+    {
+        return $book->createBook();
+    }
+    
+    public function deleteBook(Book $book)
+    {
+        return $book->deleteBook();
+    }
+    
+    public function updateBook(Book $book)
+    {
+        return $book->updateBook();
+    }
+    
+    public function getCategoriesForBook($book_id)
+    {
+        $book = new Book();
+        $book->loadBookByID($book_id);
+        return $book->categories;
+    }
+    
+    public function deleteBookCategory(Category $category, Book $book)
+    {
+        $sql = "DELETE FROM book_category WHERE category_id = $category->id AND book_id = $book->id";
+        return $this->conn->statement($sql);
+    }
+    
+    public function addCategoryToBook(Category $category, Book $book)
+    {
+        $sql = "INSERT INTO book_category(book_id, category_id) VALUES($book->id, $category->id)";
+        return $this->conn->statement($sql);
+    }
+   
 }
+?>
--- BookStore/Control/Order_controller.php
+++ BookStore/Control/Order_controller.php
@@ -1,5 +1,9 @@
 <?php
-
+include_once '../Model/Book.php';
+include_once '../Model/Category.php';
+include_once '../Model/Order.php';
+include_once '../Model/Manager.php';
+include_once '../Model/BookOrder.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,5 +16,71 @@
  * @author Christian
  */
 class Order_controller {
-    //put your code here
+    private $conn;
+    public function __construct() {
+        $this->conn = new DBConnection();
+    }
+    
+    public function getOrder($id)
+    {
+        $order = new Order();
+        $order->loadOrder($id);
+        return $order;
+    }
+    
+    public function getOrderDetails(Order $order)
+    {
+        
+    }
+
+    public function deleteOrder(Order $order)
+    {
+        $book_order = new BookOrder();
+        if($book_order->deleteBookOrder() && $order->deleteOrder())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public function createOrder(Order $order, ArrayObject $bookOrders)
+    {
+        $success = FALSE;
+        $order_id = $order->createOrder();
+        if($order_id == 0)
+        {
+            return $success;
+        }
+        
+        foreach ($bookOrders as $b)
+        {
+            $b->order_id = $order_id;
+            if($b->createBookOrder() == FALSE)
+            {
+                return $success;
+            }
+        }
+        $success = TRUE;
+        return $success;
+    }
+    /**
+     * Update things like name and address.
+     * @param Order $order
+     * @return boolean
+     */
+    public function updateOrder(Order $order)
+    {
+        return $order->updateOrder();
+    }
+    /**
+     * Updates counts of books and books ordered.
+     * @param BookOrder $book_order Instance of BookOrder with the new count of books ordered
+     */
+    public function updateOrderDetails(BookOrder $book_order)
+    {
+        return $book_order->updateBookOrder();
+    }
 }
