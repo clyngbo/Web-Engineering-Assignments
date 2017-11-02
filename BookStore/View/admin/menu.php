9cf6e44b26cbbbc6b9b4cfb57d071ba7b0eb8ece
lyngbo1109 <31726129+lyngbo1109@users.noreply.github.com>
2017-11-02 07:58:33 +0100

    CMS menu and menu CSS

--- BookStore/View/admin/menu.php
+++ BookStore/View/admin/menu.php
@@ -6,3 +6,39 @@
  * and open the template in the editor.
  */
 
+
+
+?>
+
+<nav class="navClass" >
+    <ul>
+        
+                
+            <li>Books<ul class="sub-menu">
+                    <li><a href="CreateBook.php">Create Book</a></li>
+                    <li><a href="UpdateBook.php">Update Book</a></li>
+                    <li><a href="DeleteBook.php">Delete Book</a></li>
+                    <li><a href="Books.php">View Books</a></li>
+            </ul></li>
+            
+            <li>Categories<ul class="sub-menu">
+                    <li><a href="CreateCategory.php">Create Category</a></li>
+                    <li><a href="UpdateCategory.phpa">Update Category</a></li>
+                    <li><a href="DeleteCategory.php">Delete Category</a></li>
+            </ul></li>
+            
+            <li>Orders<ul class="sub-menu">
+                    <li><a href="CreateOrder.php">Create Order</a></li>
+                    <li><a href="UpdateOrder.php">Update Order</a></li>
+                    <li><a href="DeleteOrder.php">Delete Order</a></li>
+                    <li><a href="Orders.php">View Orders</a></li>
+            </ul></li>
+            
+            <li>Managers<ul class="sub-menu">
+                    <li><a href="CreateManager.php">Create Manager</a></li>
+                    <li><a href="UpdateManager.php">Update Manager</a></li>
+                    <li><a href="DeleteManager.php">Delete Manager</a></li>
+            </ul></li>
+        </ul>
+</nav>
+<br/><br/><br/><br/>
